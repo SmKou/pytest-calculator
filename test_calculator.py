b39721d25eff50741e4113beb9e3672c0866164a
Sm Kou <66178856+SmKou@users.noreply.github.com>
2023-10-11 14:20:21 -0700

    Change tests to parameterized tests

--- test_calculator.py
+++ test_calculator.py
@@ -1,53 +1,61 @@
 import pytest
 from calculator import Calculator
 
-def test_add_positive_numbers():
+def test_calculator_add():
+    input = {
+        3: [1, 2],
+        0: [2, -2],
+        -1: [-3, 2],
+        -3: [-1, -2]
+    }
     calculator = Calculator()
-    assert calculator.add(1, 2) == 3
+    for k in input:
+        a = input[k][0]
+        b = input[k][1]
+        assert calculator.add(a, b) == k
 
-def test_add_positive_and_negative():
+def test_calculator_subtract():
+    input = {
+        2: [3, 1],
+        -2: [1, 3],
+        3: [2, -1],
+        -3: [-1, 2],
+        -1: [-2, -1],
+        1: [-1, -2]
+    }
     calculator = Calculator()
-    assert calculator.add(2, -2) == 0
+    for k in input:
+        a = input[k][0]
+        b = input[k][1]
+        assert calculator.subtract(a, b) == k
 
-def test_add_negative_numbers():
+def test_calculator_multiply():
+    input = {
+        6: [2, 3],
+        -6: [-2, 3],
+        -4: [2, -2],
+        4: [-2, -2]
+    }
     calculator = Calculator()
-    assert calculator.subtract(-2, -1) == -1
+    for k in input:
+        a = input[k][0]
+        b = input[k][1]
+        assert calculator.multiply(a, b) == k
 
-def test_subtract_positive_numbers():
+def test_calculator_divide():
+    input = {
+        2: [6, 3],
+        0.5: [3, 6],
+        -4: [8, -2],
+        -2: [-6, 3],
+        3: [-6, -2],
+        0.25: [-1, -4]
+    }
     calculator = Calculator()
-    assert calculator.subtract(3, 1) == 2
-
-def test_subtract_positive_and_negative():
-    calculator = Calculator()
-    assert calculator.subtract(2, -1) == 3
-
-def test_subtract_negative_numbers():
-    calculator = Calculator()
-    assert calculator.subtract(-2, -1) == -1
-
-def test_multiply_positive_numbers():
-    calculator = Calculator()
-    assert calculator.multiply(2, 3) == 6
-
-def test_multiply_positive_and_negative():
-    calculator = Calculator()
-    assert calculator.multiply(-2, 3) == -6
-
-def test_multiply_negative_numbers():
-    calculator = Calculator()
-    assert calculator.multiply(-2, -3) == 6
-
-def test_divide_positive_numbers():
-    calculator = Calculator()
-    assert calculator.divide(6, 3) == 2
-
-def test_divide_positive_and_negative():
-    calculator = Calculator()
-    assert calculator.divide(6, -3) == -2
-
-def test_divide_negative_numbers():
-    calculator = Calculator()
-    assert calculator.divide(-6, -2) == 3
+    for k in input:
+        a = input[k][0]
+        b = input[k][1]
+        assert calculator.divide(a, b) == k
 
 def test_divide_raise_exception_on_zero_second_arg():
     calculator = Calculator()
