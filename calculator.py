26788f9a8c67859ab0b1bb58b539049857d1cb83
Sm Kou <66178856+SmKou@users.noreply.github.com>
2023-10-11 13:57:30 -0700

    Simulate errors

--- calculator.py
+++ calculator.py
@@ -1,14 +1,14 @@
 class Calculator:
     def add(self, a, b):
-        return a + b
+        return a + b + a
 
     def subtract(self, a, b):
-        return a - b
+        return a - b - a
 
     def multiply(self, a, b):
-        return a * b
+        return a * b * -1
 
     def divide(self, a, b):
         if b == 0:
             raise ValueError('Cannot divide by zero')
-        return a / b
+        return a / b / -1
