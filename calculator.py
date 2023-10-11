afae8e6b9ce585169102c2b2a33956d437218e0c
Sm Kou <66178856+SmKou@users.noreply.github.com>
2023-10-11 13:58:23 -0700

    Remove errors

--- calculator.py
+++ calculator.py
@@ -1,14 +1,14 @@
 class Calculator:
     def add(self, a, b):
-        return a + b + a
+        return a + b
 
     def subtract(self, a, b):
-        return a - b - a
+        return a - b
 
     def multiply(self, a, b):
-        return a * b * -1
+        return a * b
 
     def divide(self, a, b):
         if b == 0:
             raise ValueError('Cannot divide by zero')
-        return a / b / -1
+        return a / b
